d01d4a1b07f0dbf7527828e8e4c7fb618b8ebd43
sivabalakrishnan1295 <sivabalakrishnan1295@gmail.com>
2021-04-08 15:25:24 +0530

    Code Base Structure

---
+++ api/src/test/java/com/development/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.development.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
